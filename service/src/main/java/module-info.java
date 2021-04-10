2f597a54fe245331ecf0bdf5cdfcd108ff00ff1b
Kai.He <kai.he.0811@qq.com>
2021-04-10 12:12:58 +0800

    将 service 中的模块对位进行暴露

--- service/src/main/java/module-info.java
+++ service/src/main/java/module-info.java
@@ -2,4 +2,8 @@ open module hk.qingke.learn.usermanager.service {
     requires hk.qingke.learn.usermanager.entity;
 
     requires org.apache.commons.lang3;
+
+    exports hk.qingke.learn.usermanager.service;
+    exports hk.qingke.learn.usermanager.service.exception;
+    exports hk.qingke.learn.usermanager.service.gateway.repository;
 }
