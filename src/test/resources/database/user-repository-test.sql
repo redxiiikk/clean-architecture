e64584ce4fa0fb5195262ae89c7ea3e2cceee16b
Kai.He <kai.he.0811@qq.com>
2021-04-12 00:15:03 +0800

    在单元测试时禁用 flyway & 禁止框架自动装配 H2 数据库而是使用配置文件中的 H2 的配置

--- src/test/resources/database/user-repository-test.sql
+++ src/test/resources/database/user-repository-test.sql
@@ -1,2 +1,10 @@
+CREATE TABLE IF NOT EXISTS `t_user`
+(
+    `id`       BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `username` VARCHAR(20)  NOT NULL,
+    `password` VARCHAR(100) NOT NULL,
+    `email`    VARCHAR(50)  NOT NULL
+);
+
 INSERT INTO `t_user` (`username`, `password`, `email`)
-VALUES ('Tome', '123@Ab789', 'test-tom@test.hk');
+VALUES ('Tom', '123@Ab789', 'test-tom@test.hk');
