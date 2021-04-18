25c9795c8c11a461d40e84a3f40fd051b90b34a5
Kai.He <kai.he.0811@qq.com>
2021-04-18 23:11:38 +0800

    修改单元测试的函数名称，符合二段式命名

--- domain/src/test/kotlin/hk/qingke/kotlin/usermanager/domain/UserEntityTest.kt
+++ domain/src/test/kotlin/hk/qingke/kotlin/usermanager/domain/UserEntityTest.kt
@@ -17,28 +17,28 @@ internal class UserEntityTest {
     // region username exception unit test
 
     @Test
-    fun `username length is less than the minimum length`() {
+    fun `should throw username illegal exception given username is less than min length`() {
         Assertions.assertThrows(UsernameIllegalException::class.java) {
             UserEntity("tom", "12345678@Ab", "test@test.hk")
         }
     }
 
     @Test
-    fun `username length should be greater than the maximum length`() {
+    fun `should throw username illegal exception given username is greater than max length`() {
         Assertions.assertThrows(UsernameIllegalException::class.java) {
             UserEntity("tom-test-too-too-long", "12345678@Ab", "test@test.hk")
         }
     }
 
     @Test
-    fun `username start with number`() {
+    fun `should throw username illegal exception given username start with number`() {
         Assertions.assertThrows(UsernameIllegalException::class.java) {
             UserEntity("1tom123", "12345678@Ab", "test@test.hk")
         }
     }
 
     @Test
-    fun `username contain illegal char`() {
+    fun `should throw username illegal exception given username contain illegal char`() {
         Assertions.assertThrows(UsernameIllegalException::class.java) {
             UserEntity("tom-test@", "12345678@Ab", "test@test.hk")
         }
@@ -47,21 +47,21 @@ internal class UserEntityTest {
 
     // region password exception unit test
     @Test
-    fun `password just contain number`() {
+    fun `should throw password illegal exception given password just contain number`() {
         Assertions.assertThrows(PasswordIllegalException::class.java) {
             UserEntity("tom-test", "12345678", "test@test.hk")
         }
     }
 
     @Test
-    fun `password length is less than the minimum length`() {
+    fun `should throw password illegal exception given password is less than min length`() {
         Assertions.assertThrows(PasswordIllegalException::class.java) {
             UserEntity("tom-test", "1234@Ab", "test@test.hk")
         }
     }
 
     @Test
-    fun `password length should be greater than the maximum length`() {
+    fun `should throw password illegal exception given password is greater than max length`() {
         Assertions.assertThrows(PasswordIllegalException::class.java) {
             UserEntity("tom-test", "12345678901234567890@Ab", "test@test.hk")
         }
@@ -70,14 +70,14 @@ internal class UserEntityTest {
 
     // region email exception unit test
     @Test
-    fun `email not contain @`() {
+    fun `should throw password illegal exception given email not contain @`() {
         Assertions.assertThrows(EmailIllegalException::class.java) {
             UserEntity("tom-test-123_test", "12345678@Ab", "test.test.hk")
         }
     }
 
     @Test
-    fun `email contain illegal char`() {
+    fun `should throw password illegal exception given email contain illegal char`() {
         Assertions.assertThrows(EmailIllegalException::class.java) {
             UserEntity("tom-test-123_test", "12345678@Ab", "test@test@test.hk")
         }
